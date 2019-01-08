071de0f936e540e4d1cba0b6284cba946d9cc7b5
micherlane <44877106+micherlane@users.noreply.github.com>
2019-01-08 20:53:35 -0200

    Update admin.py

--- Aprof/admin.py
+++ Aprof/admin.py
@@ -1,5 +1,16 @@
 
 from django.contrib import admin
+# Register your models here.
+# default: "Administração do Django"
+from django.utils.timezone import now
+
+admin.site.site_header = 'Painel de Controle'
+
+# defaut: " Administração do Site"
+admin.site.index_title = 'Recursos'
+
+# default: "Site de Administração do Django"
+admin.site.site_title = 'Título do HTML do Site'
 from .models import Turma, Professor
 
 class AdminTurma(admin.TabularInline):
